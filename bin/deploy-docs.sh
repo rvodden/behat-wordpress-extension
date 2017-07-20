e39e82a0369773eed93ecfd3d16171b17726380b
Paul Gibbs <djpaul@gmail.com>
2017-07-20 15:19:44 +0100

    Add script to generate and publish website docs.

--- bin/deploy-docs.sh
+++ bin/deploy-docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ..
 pip install --user --upgrade pip
 pip install --user --upgrade pymdown-extensions pygments mkdocs mkdocs-material
+pip show mkdocs-material | grep -E ^Version
 mkdocs gh-deploy
