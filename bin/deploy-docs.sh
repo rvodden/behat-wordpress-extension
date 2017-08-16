ce75fd3458a51c311ff6f4f675c537265d8c4e9e
Paul Gibbs <djpaul@gmail.com>
2017-08-16 17:46:40 +0100

    Site deploy script: clean-up afterwards.

--- bin/deploy-docs.sh
+++ bin/deploy-docs.sh
@@ -4,3 +4,4 @@ pip install --user --upgrade pip
 pip install --user --upgrade pymdown-extensions pygments mkdocs mkdocs-material pymdown-extensions
 pip show mkdocs-material | grep -E ^Version
 mkdocs gh-deploy
+rm -fr ./site/
