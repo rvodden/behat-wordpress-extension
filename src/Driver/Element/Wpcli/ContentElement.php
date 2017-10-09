a92b5888fb8f628bccc8f2c4a43dbd2a7ef0abe4
Paul Gibbs <djpaul@gmail.com>
2017-10-09 18:36:19 +0100

    Fix creating content with terms or meta
    Fixes #135

--- src/Driver/Element/Wpcli/ContentElement.php
+++ src/Driver/Element/Wpcli/ContentElement.php
@@ -19,12 +19,35 @@ class ContentElement extends BaseElement
      */
     public function create($args)
     {
+        $meta_args = [];
+        $tax_args  = [];
+
+        /*
+         * Array of taxonomy terms keyed by their taxonomy name, which WP-CLI can't handle.
+         *
+         * See https://github.com/wp-cli/entity-command/issues/44#issuecomment-325957179
+         */
+        if (isset($args['tax_input'])) {
+            $tax_args = $args['tax_input'];
+            unset($args['tax_input']);
+        }
+
+        /*
+         * Array of post meta values keyed by their post meta key, which WP-CLI can't handle.
+         *
+         * See https://github.com/wp-cli/entity-command/issues/44#issuecomment-325957179
+         */
+        if (isset($args['meta_input'])) {
+            $meta_args = $args['meta_input'];
+            unset($args['meta_input']);
+        }
+
         $wpcli_args = buildCLIArgs(
             array(
                 'ID', 'post_author', 'post_date', 'post_date_gmt', 'post_content', 'post_content_filtered', 'post_title',
                 'post_excerpt', 'post_status', 'post_type', 'comment_status', 'ping_status', 'post_password', 'post_name',
                 'to_ping', 'pinged', 'post_modified', 'post_modified_gmt', 'post_parent', 'menu_order', 'post_mime_type',
-                'guid', 'post_category', 'tax_input', 'meta_input',
+                'guid', 'post_category',
             ),
             $args
         );
@@ -32,14 +55,40 @@ class ContentElement extends BaseElement
         array_unshift($wpcli_args, '--porcelain');
         $post_id = (int) $this->drivers->getDriver()->wpcli('post', 'create', $wpcli_args)['stdout'];
 
+        // Apply taxonomy values.
+        if ($tax_args) {
+            foreach ($tax_args as $taxonomy => $terms) {
+                $args = [
+                    $post_id,
+                    $taxonomy,
+                    escapeshellarg($terms),
+                ];
+
+                $test = $this->drivers->getDriver()->wpcli('post', 'term set', $args)['stdout'];
+            }
+        }
+
+        // Apply post meta values.
+        if ($meta_args) {
+            foreach ($meta_args as $meta_key => $meta_value) {
+                $args = [
+                    $post_id,
+                    $meta_key,
+                    escapeshellarg($meta_value),
+                ];
+
+                $this->drivers->getDriver()->wpcli('post', 'meta update', $args)['stdout'];
+            }
+        }
+
         return $this->get($post_id);
     }
 
     /**
      * Retrieve an item for this element.
      *
-     * @param int|string $id   Object ID.
-     * @param array      $args Optional data used to fetch an object.
+     * @param int|string $id Object ID.
+     * @param array $args Optional data used to fetch an object.
      *
      * @throws \UnexpectedValueException
      *
