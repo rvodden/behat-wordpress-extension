be35721bc9751796b99555d7a57ba81c9ed1467c
Richard Vodden <richard@vodden.com>
2017-12-16 20:13:36 +0000

    Attempt to make the docker workflow pass

---
+++ build/docker/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+# wait for mysql to come up
+while ! mysqladmin ping -h"db" --silent; do
+    echo "Waiting for mysql..."
+    sleep 5
+done
+
+# now wait for selenium (assume if the port is open then selenium is up)
+
+until nc -z -v -w30 selenium 4444
+do
+  echo "Waiting for selenium..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+vendor/bin/wp config create --path='/usr/src/wordpress' --dbhost='db' --dbname='wordpress' --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}"
+vendor/bin/wp core install --path='/usr/src/wordpress' --url='http://wordpress/' --title="Test Installation" --admin_user="admin" --admin_password="password" --admin_email="admin@example.com"
+vendor/bin/behat --format progress --config ./build/docker/behat.yml
